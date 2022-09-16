2243515a78c0b90f512846e1e9201fccabe7491a
Lreyes4 <103380089+Lreyes4@users.noreply.github.com>
2022-09-16 10:21:16 -0600

    added folders needed

---
+++ config/connections.js
@@ -0,0 +1,12 @@
+const { connect, connection } = require('mongoose');
+
+//is this okay?
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
